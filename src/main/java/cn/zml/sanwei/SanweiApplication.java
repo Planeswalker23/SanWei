ff9a75102f186daa28f779fd5bda46a5cad5a739
fanyidong <805334683@qq.com>
2019-02-15 10:18:12 +0800

    搭建基础springboot架构

--- src/main/java/cn/zml/sanwei/SanweiApplication.java
+++ src/main/java/cn/zml/sanwei/SanweiApplication.java
@@ -1,9 +1,15 @@
 package cn.zml.sanwei;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot 启动类
+ * @author nanbei
+ */
 @SpringBootApplication
+@MapperScan("cn.zml.sanwei.dao")
 public class SanweiApplication {
 
     public static void main(String[] args) {
