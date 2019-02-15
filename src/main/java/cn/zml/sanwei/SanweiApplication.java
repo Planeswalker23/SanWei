7b2dadbb245ae30cb95ae61125b86663636ed2cc
fanyidong <805334683@qq.com>
2019-02-15 09:14:53 +0800

    搭建基础springboot架构

---
+++ src/main/java/cn/zml/sanwei/SanweiApplication.java
@@ -0,0 +1,14 @@
+package cn.zml.sanwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanweiApplication.class, args);
+    }
+
+}
+
