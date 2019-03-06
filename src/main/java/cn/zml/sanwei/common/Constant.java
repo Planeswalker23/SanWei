665275050dd2da5012deaaf1e3521d00df215bd7
fanyidong <805334683@qq.com>
2019-03-06 09:19:04 +0800

    增加常量类

---
+++ src/main/java/cn/zml/sanwei/common/Constant.java
@@ -0,0 +1,18 @@
+package cn.zml.sanwei.common;
+
+/**
+ * 常量类
+ * @author fanyidong
+ * @date Created in 2019-03-06
+ */
+public interface Constant {
+
+    /**
+     * 数字常量
+     */
+    Integer TWO = 2;
+    Integer THOUSAND = 1000;
+
+    String SYSTEM_ERROR = "系统错误，请联系管理员";
+
+}
