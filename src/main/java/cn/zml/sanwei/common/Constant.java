e22abf310d3b72382ba1fbf2e453fac64e028d37
fanyidong <805334683@qq.com>
2019-03-08 23:04:51 +0800

    新建类别常量

--- src/main/java/cn/zml/sanwei/common/Constant.java
+++ src/main/java/cn/zml/sanwei/common/Constant.java
@@ -13,6 +13,21 @@ public interface Constant {
     Integer TWO = 2;
     Integer THOUSAND = 1000;
 
+    /**
+     * 图书类别
+     * 1-小说 2-经济 3-管理 4-教育 5-文化 6-历史 7-文学 8-名著 9-军事 10-美食
+     */
+    Integer STROY = 1;
+    Integer ECONOMICS = 2;
+    Integer MANAGE = 3;
+    Integer EDUCATION = 4;
+    Integer CULTURE = 5;
+    Integer HISTORY = 6;
+    Integer LITERATURE = 7;
+    Integer MASTERWORK = 8;
+    Integer MILITARY = 9;
+    Integer FOOD = 10;
+
     String SYSTEM_ERROR = "系统错误，请联系管理员";
 
 }
