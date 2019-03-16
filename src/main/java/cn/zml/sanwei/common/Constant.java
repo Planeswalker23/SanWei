459858978dce85a4498acfa41335047437db1f3c
fanyidong <805334683@qq.com>
2019-03-16 20:12:28 +0800

    增加常量

--- src/main/java/cn/zml/sanwei/common/Constant.java
+++ src/main/java/cn/zml/sanwei/common/Constant.java
@@ -14,6 +14,12 @@ public interface Constant {
     Integer THOUSAND = 1000;
 
     /**
+     * 请求状态码
+     */
+    Integer REQUEST_SUCCESS_CODE = 200;
+    Integer REQUEST_FAILED_CODE = 500;
+
+    /**
      * 图书类别
      * 1-小说 2-经济 3-管理 4-教育 5-文化 6-历史 7-文学 8-名著 9-军事 10-美食
      */
@@ -35,5 +41,23 @@ public interface Constant {
     Integer RANDOM_BOOK = 12;
 
     String SYSTEM_ERROR = "系统错误，请联系管理员";
+
+    String EDIT_FAILED = "未修改任何数据";
+
+    String USER_NOT_EXIST = "该用户不存在";
+
+    String WRONG_PASSWORD = "密码错误";
+
+    String WRONG_MAIL = "邮箱格式错误";
+
+    String MAIL_EXIST = "邮箱已被注册";
+
+    String USER_EXIST = "用户已存在";
+
+    String EMPTY_PARAMS = "缺少参数";
+
+    String SUCCESS = "成功";
+
+    String FAILED = "失败";
 
 }
